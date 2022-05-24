3884af249414b111f22f4c597db391036c7b0e16
AlessioConti <alessioconti02@yahoo.it>
2022-05-24 16:14:51 +0200

    Implementazione template Service + aggiornamenti DAO

---
+++ src/main/java/it/prova/gestioneordini/service/ArticoloService.java
@@ -0,0 +1,18 @@
+package it.prova.gestioneordini.service;
+
+import java.util.List;
+
+import it.prova.gestioneordini.dao.articolo.ArticoloDAO;
+import it.prova.gestioneordini.model.Articolo;
+
+public interface ArticoloService {
+	public List<Articolo> listAll() throws Exception;
+	
+	public Articolo caricaSingoloElemento(Long  id) throws Exception;
+	
+	public void aggiorna(Articolo categoriaInstance) throws Exception;
+	
+	public void inserisci(Articolo categoriaInstance) throws Exception;
+	
+	public void setCategoriaDAO(ArticoloDAO articoloDAO);
+}
---
+++ src/main/java/it/prova/gestioneordini/service/OrdineService.java
@@ -0,0 +1,20 @@
+package it.prova.gestioneordini.service;
+
+import java.util.List;
+
+import it.prova.gestioneordini.dao.ordine.OrdineDAO;
+import it.prova.gestioneordini.model.Ordine;
+
+public interface OrdineService {
+	
+	public List<Ordine> listAll() throws Exception;
+	
+	public Ordine caricaSingoloElemento(Long  id) throws Exception;
+	
+	public void aggiorna(Ordine ordineInstance) throws Exception;
+	
+	public void inserisci(Ordine ordineInstance) throws Exception;
+	
+	public void setOrdineDAO(OrdineDAO ordineDAO);
+	
+}
