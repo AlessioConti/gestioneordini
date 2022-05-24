6ff7003e4188660a27071aaa38a8cc743fccc806
AlessioConti <alessioconti02@yahoo.it>
2022-05-24 22:13:25 +0200

    Implementazione Test, inizio testing

--- src/main/java/it/prova/gestioneordini/test/TestOrdini.java
+++ src/main/java/it/prova/gestioneordini/test/TestOrdini.java
@@ -1,10 +1,34 @@
 package it.prova.gestioneordini.test;
 
+import it.prova.gestioneordini.service.ArticoloService;
+import it.prova.gestioneordini.service.CategoriaService;
+import it.prova.gestioneordini.service.MyServiceFactory;
+import it.prova.gestioneordini.service.OrdineService;
+import it.prova.gestioneordini.dao.EntityManagerUtil;
+
 public class TestOrdini {
 
 	public static void main(String[] args) {
+		ArticoloService articoloServiceInstance = MyServiceFactory.getArticoloServiceInstance();
+		CategoriaService categoriaServiceInstance = MyServiceFactory.getCategoriaServiceInstance();
+		OrdineService ordineServiceInstance = MyServiceFactory.getOrdineServiceInstance();
 		
-		
+		try {
+			
+			System.out.println("Nella tabella Articolo ci sono " +articoloServiceInstance.listAll().size()+ " elementi.");
+			System.out.println("Nella tabella Categoria ci sono " +categoriaServiceInstance.listAll().size()+ " elementi.");
+			System.out.println("Nella tabella Ordine ci sono " +ordineServiceInstance.listAll().size()+ " elementi.");
+			
+			System.out.println("INIZIO TESTING");
+			
+			
+			
+		}catch (Throwable e) {
+			e.printStackTrace();
+		} finally {
+
+			EntityManagerUtil.shutdown();
+		}
 		
 	}
 
