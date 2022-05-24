ac5b07ed0416676d6b7b2ed14b5388ec7506d157
AlessioConti <alessioconti02@yahoo.it>
2022-05-24 21:49:34 +0200

    Implementazione CategoriaServiceImpl semplice

--- src/main/java/it/prova/gestioneordini/service/CategoriaServiceImpl.java
+++ src/main/java/it/prova/gestioneordini/service/CategoriaServiceImpl.java
@@ -2,45 +2,115 @@ package it.prova.gestioneordini.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import it.prova.gestioneordini.dao.EntityManagerUtil;
 import it.prova.gestioneordini.dao.categoria.CategoriaDAO;
 import it.prova.gestioneordini.model.Categoria;
 
 public class CategoriaServiceImpl implements CategoriaService {
 
+	private CategoriaDAO categoriaDAO;
+	
 	@Override
 	public List<Categoria> listAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+		
+		try {
+			categoriaDAO.setEntityManager(entityManager);
+			
+			return categoriaDAO.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
 	public Categoria caricaSingoloElemento(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+
+			categoriaDAO.setEntityManager(entityManager);
+			
+			return categoriaDAO.get(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
 	public void aggiorna(Categoria categoriaInstance) throws Exception {
-		// TODO Auto-generated method stub
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
 
+		try {
+			entityManager.getTransaction().begin();
+			
+			categoriaDAO.setEntityManager(entityManager);
+			
+			categoriaDAO.update(categoriaInstance);
+			
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
 	public void inserisci(Categoria categoriaInstance) throws Exception {
-		// TODO Auto-generated method stub
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
 
+		try {
+			entityManager.getTransaction().begin();
+			
+			categoriaDAO.setEntityManager(entityManager);
+			
+			categoriaDAO.insert(categoriaInstance);
+			
+			entityManager.getTransaction().commit();
+		}catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
 	public void setCategoriaDAO(CategoriaDAO categoriaDAO) {
-		// TODO Auto-generated method stub
-
+		this.categoriaDAO = categoriaDAO;
 	}
 
 	@Override
 	public void rimuovi(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			entityManager.getTransaction().begin();
+			
+			categoriaDAO.setEntityManager(entityManager);
+			
+			categoriaDAO.delete(categoriaDAO.get(id));
+			
+			entityManager.getTransaction().commit();
+		}catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 }
